d6b678e4ad86434ce4802bb918d81b4ee47388ee
priyanshu_owner <67639480+priyanshu-7938@users.noreply.github.com>
2024-04-03 18:56:27 +0530

    Created contracts

---
+++ codehive/contracts/Question.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Question {
+    //predefiners...
+    string public QuestionData;//ipfs data goes here....
+    string public topicName;//name of the bounty....
+    address moderator;//owner of the bounty....
+    
+    //non static data...
+    uint256 public bountyValue;
+    bool public claimed = false;// initially the bounty will be unclaimed....
+    address public bountyWinner = address(0);
+    string public winnerCode;
+    mapping(address => string) codes;
+    address[] submissions;
+    string public chatHash;
+    uint16 public difficulty;
+
+    modifier onlyModerator(){
+        require(msg.sender == moderator, "Not authoresised.");
+        _;
+    }
+    
+    constructor(string memory _name, string memory _quesData, address _mod, uint16 _diff, string memory _chatHash)payable{
+        QuestionData = _quesData;
+        topicName = _name;
+        moderator = _mod;
+        //funding the question....
+        bountyValue = msg.value;
+        claimed = false;
+        difficulty = _diff;
+        chatHash=_chatHash;
+    }
+    function submitCode(string memory _codeHash, address _sender)public onlyModerator returns(bool){
+        require(bytes(codes[_sender]).length==0,"code allready submitted.");
+        codes[_sender] = _codeHash;
+        submissions.push(_sender);
+        if(claimed){
+            return false;
+        }
+        (bool se, ) = _sender.call{value: bountyValue}("");
+        claimed = se;
+        bountyWinner == _sender;
+        winnerCode = _codeHash;
+        return se;
+    }
+    
+    function haveSubmitted()public view returns(bool){
+        return (bytes(codes[msg.sender]).length != 0);
+    }
+}
