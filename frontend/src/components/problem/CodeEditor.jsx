7d4866b06b9add404f9bb147c6f9b5cf62bb9182
arman <saadabban2725@gmail.com>
2024-04-25 14:48:27 +0530

    added non-editable lines restrictrions to the code editor

--- frontend/src/components/problem/CodeEditor.jsx
+++ frontend/src/components/problem/CodeEditor.jsx
@@ -1,19 +1,17 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import CodeMirror from "@uiw/react-codemirror";
-import { vscodeDark } from "@uiw/codemirror-theme-vscode";
-import { javascript } from "@codemirror/lang-javascript";
-import { Code } from "lucide-react";
-import EditorFooter from "./EditorFooter";
-import { useRecoilState } from "recoil";
 import { resultAtom } from "@/atoms/userAtom";
+import { Editor } from "@monaco-editor/react";
+import { constrainedEditor } from "constrained-editor-plugin";
+import { Code } from "lucide-react";
+import { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
 
-const CodeEditor = ({ starterCode, setUserCode,userCode }) => {
+const CodeEditor = ({ starterCode, setUserCode, userCode }) => {
   const [result, setResult] = useRecoilState(resultAtom);
   const { pid } = useParams();
-  
+
   const handleSubmit = () => {
     setResult(true);
   };
@@ -27,10 +25,35 @@ const CodeEditor = ({ starterCode, setUserCode,userCode }) => {
     }
   }, [pid, starterCode]);
 
+  // restrictionsss
+  const editorRef = useRef(null);
+  var restrictions = [];
+
+  function handleEditorDidMount(editor, monaco) {
+    try {
+      editorRef.current = editor;
+      const constrainedInstance = constrainedEditor(monaco);
+      const model = editor.getModel();
+      constrainedInstance.initializeIn(editor);
+      restrictions.push({
+        // range : [startLine, startColumn, endLine, endColumn];
+        range: [0, 1, 1, 1],
+        allowMultiline: true,
+      });
+      constrainedInstance.addRestrictionsTo(model, restrictions);
+    } catch (error) {
+      console.error("Failed to initialize editor with constraints:", error);
+    }
+  }
+
   const onChange = (value) => {
     setUserCode(value);
     localStorage.setItem(`code-${pid}`, JSON.stringify(value));
   };
+
+  if (!starterCode || starterCode.length <= 0) {
+    return <div>Loading ....</div>;
+  }
 
   return (
     <main className="relative h-full w-full overflow-auto bg-third">
@@ -45,13 +68,24 @@ const CodeEditor = ({ starterCode, setUserCode,userCode }) => {
           </div>
         </div>
       </nav>
-      <CodeMirror
+      <Editor
+        ref={editorRef.current}
+        height="85vh"
+        width={`100%`}
+        language={"javascript"}
+        onMount={handleEditorDidMount}
+        theme={"vs-dark"}
+        defaultValue="// some comment"
+        onChange={onChange}
+        value={userCode}
+      />
+      {/* <CodeMirror
         value={userCode}
         onChange={onChange}
         theme={vscodeDark}
         extensions={[javascript()]}
         style={{ fontSize: 16 }}
-      />
+      /> */}
       {/* <EditorFooter handleSubmit={handleSubmit} /> */}
     </main>
   );
