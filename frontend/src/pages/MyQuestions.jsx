c62b8251b073e065957bb8085970443ac0d9659e
priyanshu_owner <67639480+priyanshu-7938@users.noreply.github.com>
2024-05-04 04:44:46 +0530

    updated contract integration #2

--- frontend/src/pages/MyQuestions.jsx
+++ frontend/src/pages/MyQuestions.jsx
@@ -3,6 +3,16 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Nav from "@/components/problems/Nav";
 
 export default function MyQuestions() {
+  // useEffect(() => {
+  //   const getQuestions = async () => {
+  //     const contract = await window.tronLink.tronWeb.contract(
+  //       ABI_Bank,
+  //       import.meta.env.VITE_NILE_BANK_ADD,
+  //     );
+  //     const questions = await contract.questionList().call();
+  //   };
+  //   getQuestions();
+  // }, [ABI_Bank]);
   return (
     <main className=" min-h-screen w-full bg-background">
       {/* right side */}
