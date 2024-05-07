cd02c755e678e22539965f7186ec2cc53f556cac
arman <saadabban2725@gmail.com>
2024-05-07 16:26:05 +0530

    ui improvements and claimed status is dynamic now

--- frontend/src/components/problem/Nav.jsx
+++ frontend/src/components/problem/Nav.jsx
@@ -6,10 +6,6 @@ import { useNavigate } from "react-router-dom";
 const Nav = () => {
   const navigate = useNavigate();
 
-  const loginHandler = () => {
-    navigate("/login");
-  };
-  const userId = "sfdsd";
   return (
     <nav className="sticky inset-x-0 bg-background px-6 py-2">
       <main className="flex items-center justify-between py-1">
@@ -23,7 +19,11 @@ const Nav = () => {
         <div className="flex h-full w-full items-center space-x-4">
           {/* prev and next problem buttons */}
           <div className="flex items-center space-x-2 pl-4 text-lg text-foreground">
-            <ChevronLeft className="cursor-pointer" size={28} />
+            <ChevronLeft
+              className="cursor-pointer"
+              size={28}
+              onClick={() => navigate(-1)}
+            />
             <span className="text-sm text-second">{"Problem 1"}</span>
             <ChevronRight className="cursor-pointer" size={28} />
           </div>
--- frontend/src/hooks/useDeployQuestion.js
+++ frontend/src/hooks/useDeployQuestion.js
@@ -1,91 +1,108 @@
 import { useTheContext } from "@/context";
-import { useState } from 'react';
+import { useState } from "react";
 import { useWallet } from "@tronweb3/tronwallet-adapter-react-hooks";
 
-export default function useDeployQuestion(){
-    const { wallet, address, connected, select, connect, disconnect, signMessage, signTransaction } = useWallet();
-    const { ABI, QuesBYTECODE } = useTheContext(); 
-    const [ error, setError ] = useState(null);
-    const [ deployedAddress, setDeployedAddress ] = useState(null);
-    const [ deployed, setdeployed ] = useState(false);
+export default function useDeployQuestion() {
+  const {
+    wallet,
+    address,
+    connected,
+    select,
+    connect,
+    disconnect,
+    signMessage,
+    signTransaction,
+  } = useWallet();
+  const { ABI, QuesBYTECODE } = useTheContext();
+  const [error, setError] = useState(null);
+  const [deployedAddress, setDeployedAddress] = useState(null);
+  const [deployed, setdeployed] = useState(false);
 
+  const DEPLOY = (difficulty, formData, bounty) => {
+    //first deploy the ipfs thingy....
 
-    const DEPLOY = (difficulty, formData, bounty)=>{
-
-        //first deploy the ipfs thingy....
-        
-        //first deployment to pinat.. man....
-        //this goes away... man...
-        const options = {
-            method: 'POST',
-            headers: {Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`, 'Content-Type': 'application/json'},
-            body: `{"pinataOptions":{"cidVersion":1},"pinataMetadata":{"name":"${formData.name.replace(/ /g, "_")}.json"},"pinataContent":${JSON.stringify(formData)}}`
-        };
-        if(!window.tron.tronWeb.ready){
-            console.error("cant deploy tron undefined.");
-            return;
-        }
-        fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', options)
-        .then(response => response.json())
-        .then(response => response.IpfsHash)
-        .then((res)=>{
-            console.log("pinata deployed at :",res);
+    //first deployment to pinat.. man....
+    //this goes away... man...
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_BTFS_NODE}`,
+        "Content-Type": "application/json",
+      },
+      body: `{"pinataOptions":{"cidVersion":1},"pinataMetadata":{"name":"${formData.name.replace(/ /g, "_")}.json"},"pinataContent":${JSON.stringify(formData)}}`,
+    };
+    if (!window.tron.tronWeb.ready) {
+      console.error("cant deploy tron undefined.");
+      return;
+    }
+    fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", options)
+      .then((response) => response.json())
+      .then((response) => response.IpfsHash)
+      .then((res) => {
+        console.log("pinata deployed at :", res);
         // (string memory _name, string memory _quesData, address _mod, uint16 _diff, address _webHandler)
-            console.log(ABI,QuesBYTECODE);
+        console.log(ABI, QuesBYTECODE);
 
-            const options = {
-                feeLimit: 1000000000,//The maximum TRX burns for resource consumption（1TRX = 1,000,000SUN）
-                callValue: 10,//The TRX transfer to the contract for each call（1TRX = 1,000,000SUN）
-                userFeePercentage: 100,//Consume user's resource percentage. It should be an integer between [0, 100]. if 0, means it does not consume user's resource until the developer's resource has been used up
-                originEnergyLimit: 100,//The maximum resource consumption of the creator in one execution or creation
-                abi: ABI,
-                bytecode: QuesBYTECODE,
-                parameters:[formData.name, res, window.tron.tronWeb.defaultAddress.base58.toString(), difficulty, import.meta.env.VITE_WEB_HANDLER.toString()],
-                name:"Question",
-              };
-            return window.tron.tronWeb.transactionBuilder.createSmartContract(options,window.tron.tronWeb.defaultAddress.base58);
+        const options = {
+          feeLimit: 1000000000, //The maximum TRX burns for resource consumption（1TRX = 1,000,000SUN）
+          callValue: 10, //The TRX transfer to the contract for each call（1TRX = 1,000,000SUN）
+          userFeePercentage: 100, //Consume user's resource percentage. It should be an integer between [0, 100]. if 0, means it does not consume user's resource until the developer's resource has been used up
+          originEnergyLimit: 100, //The maximum resource consumption of the creator in one execution or creation
+          abi: ABI,
+          bytecode: QuesBYTECODE,
+          parameters: [
+            formData.name,
+            res,
+            window.tron.tronWeb.defaultAddress.base58.toString(),
+            difficulty,
+            import.meta.env.VITE_WEB_HANDLER.toString(),
+          ],
+          name: "Question",
+        };
+        return window.tron.tronWeb.transactionBuilder.createSmartContract(
+          options,
+          window.tron.tronWeb.defaultAddress.base58,
+        );
 
+        // const pro = window.tron.tronWeb.contract().new({
+        //     abi: ABI,
+        //     bytecode: QuesBYTECODE,
+        //     feeLimit: 40000000000,
+        //     callValue: parseInt(bounty)* 1000000,
+        //     userFeePercentage: 100,
+        //     originEnergyLimit: 1e7,
+        //     parameters: [formData.name, res, window.tron.tronWeb.defaultAddress.base58.toString(), difficulty, import.meta.env.VITE_WEB_HANDLER.toString()],
+        // });
+        // pro.then((res)=>{console.log("something",res)}).catch(err=>console.log(err));
+        // throw new Error("not implemented yet");
+        // return window.tron.tronWeb.contract().new({
+        //     abi: ABI,
+        //     bytecode: QuesBYTECODE,
+        //     feeLimit: 40000000000,
+        //     callValue: parseInt(bounty)* 1000000,
+        //     userFeePercentage: 100,
+        //     originEnergyLimit: 1e7,
+        //     parameters: [formData.name, res, window.tron.tronWeb.defaultAddress.base58.toString(), difficulty, import.meta.env.VITE_WEB_HANDLER.toString()],
+        // });
+      })
+      .then((contract) => {
+        console.log("the contract made =>", contract);
+        //signing the txn...
+        return window.tron.tronWeb.trx.sign(contract);
+      })
+      .then((signedTransaction) => {
+        console.log("the txn is signed.... =>", signedTransaction);
+        return window.tron.tronWeb.trx.sendRawTransaction(signedTransaction);
+      })
+      .then((resultBaby) => {
+        console.log("result =>", resultBaby);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("error occured!");
+        setError(err);
+      });
+  };
 
-
-            const pro = window.tron.tronWeb.contract().new({
-                abi: ABI,
-                bytecode: QuesBYTECODE,
-                feeLimit: 40000000000,
-                callValue: parseInt(bounty)* 1000000,
-                userFeePercentage: 100,
-                originEnergyLimit: 1e7,
-                parameters: [formData.name, res, window.tron.tronWeb.defaultAddress.base58.toString(), difficulty, import.meta.env.VITE_WEB_HANDLER.toString()],
-            });
-            pro.then((res)=>{console.log("something",res)}).catch(err=>console.log(err));
-            throw new Error("not implemented yet");
-            return window.tron.tronWeb.contract().new({
-                abi: ABI,
-                bytecode: QuesBYTECODE,
-                feeLimit: 40000000000,
-                callValue: parseInt(bounty)* 1000000,
-                userFeePercentage: 100,
-                originEnergyLimit: 1e7,
-                parameters: [formData.name, res, window.tron.tronWeb.defaultAddress.base58.toString(), difficulty, import.meta.env.VITE_WEB_HANDLER.toString()],
-            });
-        })
-        .then((contract)=>{
-            console.log("the contract made =>", contract);
-            //signing the txn...
-            return window.tron.tronWeb.trx.sign(contract);
-        })
-        .then(( signedTransaction )=>{
-            console.log("the txn is signed.... =>", signedTransaction);
-            return window.tron.tronWeb.trx.sendRawTransaction(signedTransaction);           
-        })
-        .then((resultBaby)=>{
-            console.log("result =>",resultBaby);
-        })
-            .catch((err)=>{
-                console.error(err);
-                alert("error occured!");
-                setError(err);
-            })
-        }
-            
-    return {DEPLOY, deployed, deployedAddress, error}
-} 
+  return { DEPLOY, deployed, deployedAddress, error };
+}
